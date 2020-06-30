add040c0f4a180a9eabb7a46b48869cbb048d4b7
rshekar005 <rshekar005@gmail.com>
2020-06-30 21:46:22 +0530

    added a common method to take screenshot and get current date , time method

--- src/main/java/com/crm/qa/utils/TestUtil.java
+++ src/main/java/com/crm/qa/utils/TestUtil.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.commons.compress.archivers.dump.InvalidFormatException;
 import org.apache.commons.io.FileUtils;
@@ -12,8 +14,10 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.io.FileHandler;
 
 import com.crm.qa.base.TestBase;
 
@@ -42,6 +46,30 @@ public class TestUtil extends TestBase {
 		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		String currentDir = System.getProperty("user.dir");
 		FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
+	}
+	public String screenshot(String screenshotname,WebDriver driver)
+	{
+		String destination=null;
+	    File srcFile= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		try {
+		destination=System.getProperty("user.dir")+"/Screenshot/"+screenshotname+dateandtime()+".png";
+	        FileHandler.copy(srcFile, new File(destination));
+			//FileUtils.copyFile(srcFile, new File(destination));
+			System.out.println("Screenshot Taken");
+		} catch (IOException e) {
+			System.out.println("Screenshot Failed");
+			e.printStackTrace();
+			
+		}
+		return destination;
+	}
+	public static String dateandtime()
+	{
+		SimpleDateFormat formatter = new SimpleDateFormat("dd_MM_yyyy_HH_mm_ss");  
+	    Date date = new Date();  
+	    String dateandtime= formatter.format(date);
+	    System.out.println(dateandtime);
+		return dateandtime;
 	}
 
 }
